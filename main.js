ff81255e8b9fd373eb1dc3ad4f0907fa07cc65dd
Fernando Franco <fernandofr077@gmail.com>
2021-01-06 05:30:09 -0300

    Criado o layout de horas e datas

---
+++ main.js
@@ -0,0 +1,74 @@
+const relogio = document.getElementById('relogio');
+const data = document.getElementById('data');
+const comecar = document.getElementById('comecar');
+
+
+const horario = ()=>{
+    let d = new Date();
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+    h<10?h='0'+h:h
+    m<10?m='0'+m:m
+    s<10?s='0'+s:s
+    relogio.innerText = h+':'+m+':'+s
+}
+const datas = ()=>{
+    let d = new Date();
+    let diasS = d.getDay();
+    let diaM = d.getDate();
+    let m = d.getMonth();
+    let a = d.getFullYear();
+    switch(diasS){
+        case 0: diasS ='Domingo'
+        break;
+        case 1: diasS ='Segunda-Feira'
+        break;
+        case 2: diasS ='Terça-Feira'
+        break;
+        case 3: diasS ='Quarta-Feira'
+        break;
+        case 4: diasS ='Quinta-Feira'
+        break;
+        case 5: diasS ='Sexta-Feira'
+        break;
+        case 6: diasS ='Sabado'
+    }
+    switch(m){
+        case 0: m = 'Janeiro'
+        break;
+        case 1: m = 'Fevereiro'
+        break;
+        case 2: m = 'Março'
+        break;
+        case 3: m = 'Abril'
+        break;
+        case 4: m = 'Maio'
+        break;
+        case 5: m = 'Junho'
+        break;
+        case 6: m = 'Julho'
+        break;
+        case 7: m = 'Agosto'
+        break;
+        case 8: m = 'Setembro'
+        break;
+        case 9: m = 'Outubro'
+        break;
+        case 10: m = 'Novembro'
+        break;
+        case 11: m = 'Dezembro'
+        break;
+    }
+    data.innerText = diasS+', '+diaM+' de '+m+' de '+a
+
+    
+
+    console.log(diasS)
+}
+
+window.setInterval(horario,1000)
+window.onload = ()=>{
+    horario();
+    datas();
+}
